2e8508d358a1b21c5973e683f7286b57ed94f008
anbreaker <antunez19@gmail.com>
2021-04-06 20:32:14 +0200

    sacada configuracion de yargs de app

--- 03-Node-Bases/multiplicarColors/app.js
+++ 03-Node-Bases/multiplicarColors/app.js
@@ -1,22 +1,6 @@
 const { boolean } = require('yargs');
 const { crearArchivo } = require('./utils/multiply');
-
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true,
-  })
-  .option('l', {
-    alias: 'listar',
-    type: 'boolean',
-    demandOption: true,
-    default: false,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.base)) throw 'La base ha de ser un número';
-    return true;
-  }).argv;
+const { argv } = require('./config/yargsConfig');
 
 console.clear();
 
