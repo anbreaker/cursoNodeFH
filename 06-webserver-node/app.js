c6e7a9d297d8a7c091f8510d2b75ec0764ec58b9
anbreaker <antunez19@gmail.com>
2021-04-10 18:41:42 +0200

    usando hbs y cambiando una plantilla

--- 06-webserver-node/app.js
+++ 06-webserver-node/app.js
@@ -1,19 +1,42 @@
 const express = require('express');
-const app = express();
+const hbs = require('hbs');
+require('dotenv').config();
 
-const port = 3000;
+const app = express();
+const port = process.env.PORT;
+
+// Middleware
+// Express Handlebars engine
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido Estatico
+app.use(express.static('public'));
 
 //Routes Inicio
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('home', {
+    nombre: 'anbreaker',
+    titulo: 'Curso Node FH',
+  });
 });
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hola Mundo en ruta');
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: 'anbreaker',
+    titulo: 'Curso Node FH',
+  });
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    nombre: 'anbreaker',
+    titulo: 'Curso Node FH',
+  });
 });
 
 app.get('*', (req, res) => {
-  res.send('404 | Page Not Found');
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
