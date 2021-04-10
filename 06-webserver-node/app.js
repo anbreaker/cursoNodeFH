20cb0e0112a86f77bb3926f9607c8bbbe97df22b
anbreaker <antunez19@gmail.com>
2021-04-10 11:59:28 +0200

    iniciando con express rutas

--- 06-webserver-node/app.js
+++ 06-webserver-node/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+const port = 3000;
+
+//Routes Inicio
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.get('/hola-mundo', (req, res) => {
+  res.send('Hola Mundo en ruta');
+});
+
+app.get('*', (req, res) => {
+  res.send('404 | Page Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
