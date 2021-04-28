c8c864acc2a45f54be70a5fbc8fd0d350514ccfe
anbreaker <antunez19@gmail.com>
2021-04-28 21:18:26 +0200

    search by id init

--- 07-restserver-node/controllers/search.controller.js
+++ 07-restserver-node/controllers/search.controller.js
@@ -1,9 +1,42 @@
 const { response, request } = require('express');
+const { ObjectId } = require('mongoose').Types;
+
+const User = require('../models/user.model');
+const Category = require('../models/category.model');
+const Product = require('../models/product.model');
+
+const collectionsAllowed = ['users', 'category', 'products', 'roles'];
+
+const searchUser = async (term = '', res = response) => {
+  const isMongoId = ObjectId.isValid(term); // true
+
+  if (isMongoId) {
+    const user = await User.findById(term);
+    res.json({ results: user ? [user] : [] });
+  }
+};
 
 const search = async (req = request, res = response) => {
   const { collection, term } = req.params;
 
-  res.json({ collection, term });
+  if (!collectionsAllowed.includes(collection))
+    return res
+      .status(400)
+      .json({ msg: `Collections allowed to search ${collectionsAllowed}` });
+
+  switch (collection) {
+    case 'users':
+      searchUser(term, res);
+      break;
+    case 'category':
+      //
+      break;
+    case 'products':
+      //
+      break;
+    default:
+      res.status(500).json({ msg: 'Forget to search...' });
+  }
 };
 
 module.exports = { search };
