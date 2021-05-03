fa25c01ac84ef66ef24e19d596d3e3a8cfd6a46a
anbreaker <antunez19@gmail.com>
2021-05-03 18:10:30 +0200

    upload file by collection

--- 07-restserver-node/controllers/uploads.controller.js
+++ 07-restserver-node/controllers/uploads.controller.js
@@ -1,5 +1,8 @@
 const { response, request } = require('express');
+
 const { uploadFile } = require('../helpers/uploadFiles');
+const User = require('../models/user.model');
+const Product = require('../models/product.model');
 
 const loadFiles = async (req = request, res = response) => {
   //name obj postman (file)
@@ -21,7 +24,33 @@ const loadFiles = async (req = request, res = response) => {
 const updateFile = async (req = request, res = response) => {
   const { collection, id } = req.params;
 
-  res.json({ collection, id });
+  let model;
+
+  switch (collection) {
+    case 'users':
+      model = await User.findById(id);
+
+      if (!model)
+        return res.status(400).json({ msg: `There is not User with the '${id}'.` });
+      break;
+
+    case 'products':
+      model = await Product.findById(id);
+
+      if (!model)
+        return res.status(400).json({ msg: `There is not Product with the '${id}'.` });
+      break;
+
+    default:
+      return res.status(500).json({ msg: 'Shit!, Forget to validate...' });
+  }
+
+  const name = await await uploadFile(req.files, undefined, collection);
+  model.img = name;
+
+  await model.save();
+
+  res.json({ model });
 };
 
 module.exports = { loadFiles, updateFile };
