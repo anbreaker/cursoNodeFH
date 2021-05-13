c249e36f2a9edec8dd2575e3571fb4aac630e3e5
anbreaker <antunez19@gmail.com>
2021-05-13 10:47:50 +0200

    atendiendo tickets

--- 09-socket-colas/public/js/desktop.js
+++ 09-socket-colas/public/js/desktop.js
@@ -1,1 +1,48 @@
-console.log('Desktop HTML');
+// References HTMLI Elements
+const lblDesktop = document.querySelector('h1');
+const lblTicket = document.querySelector('small');
+const btnAttend = document.querySelector('button');
+const divAlert = document.querySelector('.alert');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('desktop')) {
+  window.location = 'index.html';
+
+  throw new Error('Desktop is Mandatory');
+}
+
+const desktop = searchParams.get('desktop');
+lblDesktop.innerHTML = desktop;
+divAlert.style.display = 'none';
+
+// Connect to client
+const socketClient = io();
+// console.log('socketClient');
+
+socketClient.on('connect', () => {
+  // console.log('Connect Server');
+  btnAttend.disabled = false;
+});
+
+socketClient.on('disconnect', () => {
+  // console.log('Disconnect Server');
+  btnAttend.disabled = true;
+});
+
+socketClient.on('last-ticket', (last) => {
+  // lblNewTicket.innerText = `Ticket ${last}`;
+});
+
+btnAttend.addEventListener('click', () => {
+  const payload = { desktop };
+
+  socketClient.emit('attend-ticket', payload, ({ ok, ticket, msg }) => {
+    if (!ok) {
+      lblTicket.innerText = `No More Tickets`;
+      return (divAlert.style.display = '');
+    }
+
+    lblTicket.innerText = `Ticket ${ticket.number}`;
+  });
+});
--- 09-socket-colas/socketsController/socketController.js
+++ 09-socket-colas/socketsController/socketController.js
@@ -12,6 +12,31 @@ const socketController = (socket) => {
 
     // TODO Notificar ticket pendiente
   });
+
+  socket.on('attend-ticket', ({ desktop }, callback) => {
+    if (!desktop) {
+      return callback({
+        ok: false,
+        msg: 'Desktop is Mandatory',
+      });
+    }
+
+    const ticket = ticketControl.attendTicket(desktop);
+
+    // TODO: Notificar cambio en last4
+
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: 'There are no more tickets pending',
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
+  });
 };
 
 module.exports = { socketController };
